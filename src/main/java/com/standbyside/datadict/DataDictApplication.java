6c77181ff33a6c481c3145211166850f2e952627
Jerry <smart1jerry@outlook.com>
2019-12-30 13:32:23 +0800

    [feat] add jars

---
+++ src/main/java/com/standbyside/datadict/DataDictApplication.java
@@ -0,0 +1,13 @@
+package com.standbyside.datadict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDictApplication.class, args);
+	}
+
+}
